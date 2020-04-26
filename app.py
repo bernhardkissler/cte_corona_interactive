8d8bb0bf3ed42c8450bd7cf59f4e83b298bf3afb
bernhard kissler <bernhard.kissler@outlook.de>
2020-04-26 21:07:42 +0200

    fixed error?

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__, external_stylesheets=dbc.themes.BOOTSTRAP)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 server = app.server
 
